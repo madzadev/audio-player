aa9623fd5d9f72a4051a1345945cbb9237f22b04
madarsbiss <madars.biss@gmail.com>
2021-02-17 21:05:30 +0200

    add syntax highlight

--- example/src/App.js
+++ example/src/App.js
@@ -6,6 +6,9 @@ import Footer from './components/Footer'
 import { Player } from '@madzadev/audio-player'
 import '@madzadev/audio-player/dist/index.css'
 
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
 const tracks = [
   {
     url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
@@ -26,20 +29,44 @@ const tracks = [
 
 const App = () => {
   return (
-<Wrapper>
-<Header />
+    <Wrapper>
+      <Header />
       <Player tracks={tracks} />
       <h1>Installation</h1>
-      <p>npm install @madzadev/audio-player</p>
+      <SyntaxHighlighter language='javascript' style={dracula}>
+        {`npm install @madzadev/audio-player`}
+      </SyntaxHighlighter>
       <h1>Usage</h1>
-      <p>import Player from '@madzadev/audio-player'</p>
-
+      <SyntaxHighlighter language='javascript' style={dracula}>
+        {`import Player from '@madzadev/audio-player'`}
+      </SyntaxHighlighter>
+      <SyntaxHighlighter language='javascript' style={dracula}>
+        {`const tracks = [
+  {
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    title: 'Madza - AAAAAAA',
+    tags: ['edm']
+  },
+  {
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    title: 'Madza - BBBBBB',
+    tags: ['chill']
+  },
+  {
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    title: 'Madza - CCCC',
+    tags: ['bass']
+  }
+]`}
+      </SyntaxHighlighter>
       <h1>Options</h1>
       <h1>Features</h1>
-      Play/Pause Next/Previous tracks Loop audio Shuffle play Drag progress bar Volume control Clickable playlist Filter audio files based on genre Search audio files by title Responsive design
+      Play/Pause Next/Previous tracks Loop audio Shuffle play Drag progress bar
+      Volume control Clickable playlist Filter audio files based on genre Search
+      audio files by title Responsive design
       <h1>Color schemas</h1>
       <Footer />
-  </Wrapper>
+    </Wrapper>
   )
 }
 
