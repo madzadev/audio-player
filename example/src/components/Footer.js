8b84926d77ee3df5665e247bc3c994de22418db6
madarsbiss <madars.biss@gmail.com>
2021-02-18 11:08:39 +0200

    adjust layout width

--- example/src/components/Footer.js
+++ example/src/components/Footer.js
@@ -4,7 +4,9 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.wrapper}>
-      <h1>Component and audio samples created by Madza. Icons from Icons8.</h1>
+      <p>Component, example page and audio samples created by Madza. Icons from Icons8.</p>
+      <p>Contribute to open source</p>
+      <p>Listen to my music (SC and YT icons below)</p>
     </div>
   )
 }
