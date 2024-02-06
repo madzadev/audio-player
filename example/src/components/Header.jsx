01109e6c3ca6a3dda7b4f2dd135be4df33105ee1
madzadev <madars.biss@gmail.com>
2024-02-07 00:54:38 +0200

    update header

--- example/src/components/Header.jsx
+++ example/src/components/Header.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import styles from './Header.module.css'
+import React from "react";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>Audio Player for your Music</h1>
       <p className={styles.description}>
-        Based on hooks, requires React v.16.8+
+        Based on Vite v5, requires Node v18 or newer. Tested on React 18.2.0 and
+        NextJS 14.1.0.
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
