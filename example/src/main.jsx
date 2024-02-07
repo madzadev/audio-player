ae74720de0853fc6215838e441bf9b4afc2d03de
madzadev <madars.biss@gmail.com>
2024-02-07 04:27:56 +0200

    edit react render

--- example/src/main.jsx
+++ example/src/main.jsx
@@ -1,7 +1,11 @@
 import "./index.css";
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
